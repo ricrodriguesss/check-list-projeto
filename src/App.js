a79ebb8b02fede7a5d4dc746db87d2bc1c60e887
Miguel de Freitas Muniz <miguimuniz@gmail.com>
2024-09-20 00:16:38 -0300

    refac: refatora página home e cria padrão para formato das páginas, com sidebar interativa

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from "./components/LoginPage";
+import HomePage from "./components/HomePage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </Router>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import avatar from "../assets/images/default.avatar.jpg";
+
+export const Header = ({ texto, userName }) => {
+  return (
+    <div className="flex justify-between items-center p-4 bg-gray-100">
+      <h2 className="text-lg lg:text-2xl font-semibold text-gray-700 pl-4 md:pl-6">
+        {texto}
+      </h2>
+      <div className="flex items-center space-x-4 pr-4 md:pr-6"> 
+        <img
+          className="w-10 h-10 lg:w-12 lg:h-12 rounded-full object-cover"
+          src={avatar}
+          alt="Usuário"
+        />
+        <span className="text-gray-700 text-lg lg:text-xl">{userName}</span>
+      </div>
+    </div>
+  );
+};
